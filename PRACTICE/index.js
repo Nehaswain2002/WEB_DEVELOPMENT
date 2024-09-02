34ae51ab04a3dbc24a8ced5aa0710b5d7839dbe3
Nehaswain2002 <nehaswain7@gamil.com>
2024-09-02 08:13:55 +0530

    updated

---
+++ PRACTICE/index.js
@@ -0,0 +1,11 @@
+// let symbol1= Symbol("abc");
+// let symbol2= Symbol("abc");
+//  console.log(symbol1); // Symbol(abc)
+//  console.log(symbol2); // Symbol(abc)
+let symbol1 = symbol("abc");
+let symbol2= Symbol("abc");
+console.log(symbol1);
+console.log(symbol2);
+let boolean1=true;
+let boolean2=false;
+console.log(boolean1);
