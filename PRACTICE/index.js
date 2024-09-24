7c2e61fac222047f411e87589fa9762ff3c67c54
Neha Swain <nehaswain7@gmail.com>
2024-09-24 20:52:01 +0530

    updated

--- PRACTICE/index.js
+++ PRACTICE/index.js
@@ -1,11 +1,47 @@
-// let symbol1= Symbol("abc");
-// let symbol2= Symbol("abc");
-//  console.log(symbol1); // Symbol(abc)
-//  console.log(symbol2); // Symbol(abc)
-let symbol1 = symbol("abc");
-let symbol2= Symbol("abc");
-console.log(symbol1);
-console.log(symbol2);
-let boolean1=true;
-let boolean2=false;
-console.log(boolean1);
+// for(let i=1;i<=5;i++){
+//     console.log(i);    
+// }
+// for (let i = 0; i<=4; i++)
+//     {
+//   console.log(`The no is ${i}`);
+//   }
+// for (let i=0; i<5; i++)
+//      {
+//    console.log("*");
+   
+    
+// }
+// for(let i=10;i>=1;i--)
+//  console.log(i);
+
+// let i=0;
+// while(i<5){
+// console.log("Neha");
+// i++;
+// }
+// let i=0;
+// do{
+//   console.log("neha");
+
+//   i++;
+// }while (i<5) 
+  
+// const randomNumber=math.random();
+// console.log(randomNumber);
+// let start=1;
+// let end=5;
+// const random = Math.floor(randomNumber * (end - start + 1)) + start;
+// const random = Math.floor(randomNumber * (end - start + 1)) + start;
+
+ // Step-1
+ const randomNumber = Math.random();
+ console.log(randomNumber);
+ // Step-2
+ let start = 1;
+ let end = 5;
+ // Step-3
+ // const random = Math.floor(randomNumber * (end - start + 1)) + start;
+ // Step-4
+ for (let i = 0; i < 5; i++) {
+   console.log(Math.floor(Math.random() * (end - start + 1)) + start); // randomly 1 to 5
+ }
